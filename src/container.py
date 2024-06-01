09e5ddea68a18ff35a7013f8ef2c6dd58c53b56e
Stepan Kholodov <skholodov@kamaz.digital>
2024-06-01 11:37:48 +0300

    Container

---
+++ src/container.py
@@ -0,0 +1,18 @@
+from dependency_injector.containers import DeclarativeContainer, WiringConfiguration
+from dependency_injector.providers import Singleton
+
+from database import SessionLocal
+from tickets.repos.tickets_repo import TicketsRepo
+from tickets.service.tickets_service import TicketsService
+
+
+class AppContainer(DeclarativeContainer):
+    wiring_config = WiringConfiguration(packages=["tickets"])
+
+    session_factory: Singleton = Singleton(SessionLocal)
+
+    tickets_repo: Singleton = Singleton(TicketsRepo, session_factory=session_factory)
+    tickets_service: Singleton = Singleton(
+        TicketsService,
+        repo=tickets_repo,
+    )
